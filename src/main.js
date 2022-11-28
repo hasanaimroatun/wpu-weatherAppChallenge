be6f7b45d7d362c702805054a9be54506e4c8812
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-28 13:30:49 +0700

    import the fontawesome core, font awesome icon component, specific icon, add icons to the library, add font awesome icon component

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons'
+import { dom } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).mount('#app')
+library.add({faCircleCheck})
+
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+dom.watch()
