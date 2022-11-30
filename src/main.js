7f7eeadc679d96ead0d20a9c685041b55a62c220
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-30 16:31:06 +0700

    import icon and add them to library

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCircleCheck } from '@fortawesome/free-regular-svg-icons'
+import { faLocationCrosshairs, faLocationArrow, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { dom } from '@fortawesome/fontawesome-svg-core'
 
-library.add({faCircleCheck})
+library.add({faLocationCrosshairs, faLocationArrow, faChevronRight})
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
