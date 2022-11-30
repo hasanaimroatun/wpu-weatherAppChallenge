14b43aa1858ce2170be738cf5047028ef36be55c
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-30 16:29:53 +0700

    import google fonts

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
   <div>
-    
+    <WeatherContainer />
   </div>
 </template>
 
 <script>
-
+import WeatherContainer from './components/Weather.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    WeatherContainer,
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Raleway:wght@100;500;600&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
 }
 </style>
