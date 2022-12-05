0df7646d8b139f9971b2703f637aa4ae57a18942
hasanaimroatun <imroatunhasana@gmail.com>
2022-12-05 16:09:04 +0700

    import icon location and magnifying glass and add them to library

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faLocationCrosshairs, faLocationArrow, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faLocationCrosshairs, faLocationArrow, faChevronRight, faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { dom } from '@fortawesome/fontawesome-svg-core'
 
-library.add({faLocationCrosshairs, faLocationArrow, faChevronRight})
+library.add({faLocationCrosshairs, faLocationArrow, faChevronRight, faMagnifyingGlass, faLocationDot})
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
